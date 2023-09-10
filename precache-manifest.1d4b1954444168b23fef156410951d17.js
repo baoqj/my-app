268beb9a683324136b7bd4f14b0a499a758c740f
BAO Qingjun <aibaoqj@163.com>
2023-09-10 17:00:34 -0400

    Updates

---
+++ precache-manifest.1d4b1954444168b23fef156410951d17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7f61faa96877bb7b2ab7",
+    "url": "/my-app/static/css/main.13b54e97.chunk.css"
+  },
+  {
+    "revision": "7f61faa96877bb7b2ab7",
+    "url": "/my-app/static/js/main.7f61faa9.chunk.js"
+  },
+  {
+    "revision": "3e3c3eb55c09e01a1300",
+    "url": "/my-app/static/js/1.3e3c3eb5.chunk.js"
+  },
+  {
+    "revision": "ff813772de7efb309245",
+    "url": "/my-app/static/js/2.ff813772.chunk.js"
+  },
+  {
+    "revision": "92a93f73f4a8da5c2aa8",
+    "url": "/my-app/static/js/runtime~main.92a93f73.js"
+  },
+  {
+    "revision": "da7c1a01e8a5d069427e74009bafa393",
+    "url": "/my-app/index.html"
+  }
+];
